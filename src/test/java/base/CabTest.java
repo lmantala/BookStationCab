e9543b39bb50303b25ac2ccab786b5e1ffa87872
Lucky Mantala <mantala_l@subway.com>
2022-03-20 20:36:25 +0530

    commit

--- src/test/java/base/CabTest.java
+++ src/test/java/base/CabTest.java
@@ -7,13 +7,12 @@ import pages.DashboardPage;
 public class CabTest extends Base {
 
 	public DashboardPage dashboardpage;
-
 	@Test
 	public void Test1() throws Exception {
 
 		dashboardpage = new DashboardPage(driver);
 		dashboardpage.removeAds();
 		dashboardpage.searchCab("Delhi", "Manali", "1", "12:15");
-
+		Sakshi
 	}
 }
